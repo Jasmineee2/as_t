403c5f402945b8c4e823026c03aa29513d5244d5
ippeikamimura <ippei_kamimura@icloud.com>
2021-01-13 11:02:28 +0900

    added readme

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 # as_t
+ ## 設問
+ 1. facebookとyahooのそれぞれの合計を求めなさい。
+ 2. facebookとyahooのそれぞれの合計から、cpc、cpa,ctr,コンバージョンレート,ROAS（Return On Advertising Spend）を算出してConsoleで表示しなさい。
+
+ ## Jsonパース方法
+ ```php
+ $json = file_get_contents('insight.json');
+ $json = mb_convert_encoding($json, 'UTF8');
+ $this->data = json_decode($json, true);
+ ```
+ ## パラメーター説明
+ |パラメーター名|意味|
+ |:--:|:--:|
+ |impressions|広告の表示回数|
+ |sales|広告からの売上|
+ |conversions|広告からの成約数|
+ |clicks|広告のクリック数|
+ |spend|広告の費用|
+ |platform|広告プロバイダー|
+
+ ## 統計数値説明
+ |パラメーター名|意味|
+ |:--:|:--:|
+ |CPC|cost per clickの略称で、1クリックを獲得するためにかかった費用。|
+ |CPA|cost per actionの略称で、1conversionを獲得するために掛かった費用である。|
+ |CTR|Click Through Rateの略称で、Impressionに対してclickされた比率を指す。|
+ |コンバージョンレート(CVR)|Conversion Rateの略称で、click総数に対してconversionした比率を指す。|
+ |ROAS|Return On Advertising Spendの略称で、投資した広告費用の回収率。 総売上/総コスト * 100 (%)|
+## Hot to run
 ```shell
 $ php main.php
 ```
--- main.php
+++ main.php
@@ -1,25 +1,4 @@
 <?php
-
-/**
- * 設問
- * 1. facebookとyahooのそれぞれの合計
- * 2. facebookとyahooのそれぞれの合計から、cpc、cpa,ctr,コンバージョンレート,ROAS（Return On Advertising Spend）を算出してConsoleで表示
- * 
- * 説明
- * impressions: 広告の表示回数
- * sales: 広告からの売上
- * conversions: 広告からの成約数
- * clicks: 広告のクリック数
- * spend: 広告の費用
- * platform: 広告プロバイダー
- * 
- * CPCとは、cost per clickの略称で、1クリックを獲得するためにかかった費用。
- * CPAとは、cost per actionの略称で、1conversionを獲得するために掛かった費用である。
- * CTRとは、Click Through Rateの略称で、Impressionに対してclickされた比率を指す。
- * コンバージョンレート(CVR)とは、Conversion Rateの略称で、click総数に対してconversionした比率を指す。
- * ROASとは、Return On Advertising Spendの略称で、投資した広告費用の回収率。 総売上/総コスト * 100 (%)
- */
-
 require('Insight.php');
 require('Collection.php');
 
@@ -31,9 +10,9 @@ class Main {
     
     public function __construct()
     {
-        $array = file_get_contents('insight.json');
-        $array = mb_convert_encoding($array, 'UTF8');
-        $this->data = json_decode($array, true);
+        $json = file_get_contents('insight.json');
+        $json = mb_convert_encoding($json, 'UTF8');
+        $this->data = json_decode($json, true);
     }
 
     public function displaySummaries(): void
